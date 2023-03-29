68091cd66d5e547e7415cd8e5f922943143eee37
Javokhir <javohir.murodov.9238@gmail.com>
2023-03-29 21:20:44 +0500

    changing NODE_ENV from production to development

--- dbConfig.js
+++ dbConfig.js
@@ -5,8 +5,6 @@ const { Client } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const client = new Client(
-  isProduction ? process.env.DATABASE_URL : connectionString,
-);
+const client = new Client(process.env.DATABASE_URL);
 
 module.exports = client;
